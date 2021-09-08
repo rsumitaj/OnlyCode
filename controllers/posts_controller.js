f580c56b2136dcfd0776a58c85038d47bf7792be
rsumitaj <sumitrajtech@gmail.com>
2021-09-08 21:32:45 +0530

    create more controllers and routes

---
+++ controllers/posts_controller.js
@@ -0,0 +1,3 @@
+module.exports.posts = function(req,res){
+  return res.end('<h1>OnlyCode : My First Post</h1>');
+};
---
+++ routes/posts.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const postsController = require('../controllers/posts_controller');
+
+router.get('/',postsController.posts);
+
+module.exports = router;
