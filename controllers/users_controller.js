a0bb356d7accbe099a74e3381409fc2fc7c7f865
rsumitaj <sumitrajtech@gmail.com>
2021-09-11 23:42:58 +0530

    create signUp and signIn page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,25 @@ module.exports.profile = function(req,res){
     title : 'Profile'
   });
 };
+
+module.exports.signIn = function(req,res){
+  return res.render('sign_in',{
+    title : 'SignIn'
+  });
+};
+
+module.exports.signUp = function(req,res){
+  return res.render('sign_up',{
+    title : 'SignUp'
+  });
+};
+
+// get sign up data
+module.exports.create = function(req,res){
+  // TODO Later
+};
+
+// sign in and create session for the user
+module.exports.createSession = function(req,res){
+  // TODO Later
+}
